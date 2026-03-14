abddb48eb481dcf99d25609d6f72acb964798a66
Harsh Kabra <113081552+harshk1010@users.noreply.github.com>
2026-03-14 18:47:55 +0530

    initial commit
    Changed CORS settings to allow all origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Team API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://team-page-dusky-phi.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
